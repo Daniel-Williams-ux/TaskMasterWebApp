6d9dfbf03c07f9d646540ddf740bf8ae80f8bdda
Daniel <williamsoligodaniel@gmail.com>
2024-11-24 13:10:35 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-800">
+      <h1 className="text-center text-2xl font-bold p-4">TaskMaster</h1>
+    </div>
+  );
+};
+
+export default App;
